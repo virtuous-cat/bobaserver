1848ded437ebd36236545bf8fd13198eb1cab224
Quicksilver Mercurial <quicksilver.mercurial@gmail.com>
2020-11-22 23:25:30 -0800

    wip query improvements

--- server/tags/sql/index.ts
+++ server/tags/sql/index.ts
@@ -1,9 +1,10 @@
 const getPostsWithTags = `
     select posts_with_tags.* FROM (  
         select
-            row_to_json(posts.*) as post_info,
             posts.id as post_id,
-            row_to_json(post_identity.*) as post_user_identity_info,
+            row_to_json(posts.*) as post_info,
+            row_to_json(posts_user.*) as posts_user_info,
+            row_to_json(post_identity.*) as post_thread_identity_info,
             row_to_json(post_secret_identity.*) as post_secret_indentity_info,
             ARRAY_AGG (DISTINCT tags.tag) as post_tags, 
             ARRAY_AGG (DISTINCT content_warnings.warning) as post_content_warnings,
@@ -13,7 +14,8 @@ const getPostsWithTags = `
             row_to_json(parent_thread.*) as parent_thread,
             
             row_to_json(first_post_in_thread.*) as first_post_in_thread_info,
-            row_to_json(first_post_identity) as first_post_identity_info,
+            row_to_json(first_post_in_thread_user.*) as first_post_in_thread_user_info,
+            row_to_json(first_post_identity) as first_post_thread_identity_info,
             row_to_json(first_post_secret_identity.*) as first_post_secret_indentity_info,
             ARRAY_AGG (DISTINCT first_post_tags.tag) as first_post_in_thread_tags,
             ARRAY_AGG (DISTINCT first_post_content_warnings.warning) as first_post_content_warnings,
@@ -24,7 +26,7 @@ const getPostsWithTags = `
             COALESCE(is_friend.friend, FALSE) as friend
 
         from posts
-            LEFT JOIN users as logged_in_user on logged_in_user.firebase_id  = $/firebase_id/
+            LEFT JOIN users as logged_in_user on logged_in_user.firebase_id  = 'fb2'
             LEFT JOIN LATERAL (
                SELECT true as friend 
                FROM friends 
@@ -33,6 +35,7 @@ const getPostsWithTags = `
                LIMIT 1) as is_friend ON 1=1 
 
 
+            LEFT JOIN users as posts_user on posts.author = users.id
             LEFT JOIN post_tags on posts.id = post_tags.post_id
             LEFT JOIN tags on post_tags.tag_id = tags.id
             LEFT JOIN threads as parent_thread on parent_thread.id = posts.parent_thread
@@ -43,9 +46,8 @@ const getPostsWithTags = `
             LEFT JOIN post_warnings on posts.id = post_warnings.post_id
             LEFT JOIN content_warnings on post_warnings.warning_id = content_warnings.id
             
-            
-            
             LEFT JOIN posts as first_post_in_thread on first_post_in_thread.parent_post is NULL AND first_post_in_thread.parent_thread = posts.parent_thread AND first_post_in_thread.id != posts.id
+            LEFT JOIN users as first_post_in_thread_user on first_post_in_thread.author = users.id
             LEFT JOIN post_tags as first_post_post_tags on first_post_in_thread.id = first_post_post_tags.post_id
             LEFT JOIN tags as first_post_tags on first_post_post_tags.tag_id = first_post_tags.id 
             LEFT JOIN posts as first_post_child_posts on first_post_child_posts.parent_post = first_post_in_thread.id 
@@ -67,6 +69,7 @@ const getPostsWithTags = `
             
       ) as posts_with_tags    
     WHERE
+      post_id = 2;
       post_tags @> $/includeTags/ AND
       NOT post_tags && $/excludeTags/
 `
