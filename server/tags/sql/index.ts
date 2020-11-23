a06f6e0bd12291cd992b0a503df2d7aa348a8c03
Quicksilver Mercurial <quicksilver.mercurial@gmail.com>
2020-11-23 00:19:57 -0800

    fix up query almost done part 1

--- server/tags/sql/index.ts
+++ server/tags/sql/index.ts
@@ -2,8 +2,30 @@ const getPostsWithTags = `
     select posts_with_tags.* FROM (  
         select
             posts.id as post_id,
-            row_to_json(posts.*) as post_info,
-            row_to_json(posts_user.*) as posts_user_info,
+            json_build_object(
+              'post_id', posts.string_id,
+              'author', posts.author,
+              'created', TO_CHAR(posts.created, 'YYYY-MM-DD"T"HH24:MI:SS'),
+              'content', posts.content,
+              'options', posts.options,
+              'type', posts.type,
+              'username', posts_user.username,
+              'user_avatar', posts_user.avatar_reference_id,
+              'secret_identity_name', post_secret_identity.display_name,
+              'secret_identity_avatar', post_secret_identity.avatar_reference_id,
+              'self', COALESCE(logged_in_user.id = posts.author, FALSE),
+              'friend', COALESCE(is_friend.friend, FALSE),
+              'index_tags',  ARRAY_AGG (DISTINCT tags.tag),
+              'category_tags', ARRAY_AGG (DISTINCT content_warnings.warning) 
+              
+            ) as post_info,
+            json_build_object(
+              'post_info', row_to_json(posts.*),
+              'posts_user_info', row_to_json(posts_user.*),
+              'lil_nesty', json_build_object(
+                  'posts_info', row_to_json(posts.*) 
+              )
+            ) as hurr_durr,
             row_to_json(post_identity.*) as post_thread_identity_info,
             row_to_json(post_secret_identity.*) as post_secret_indentity_info,
             ARRAY_AGG (DISTINCT tags.tag) as post_tags, 
@@ -35,7 +57,7 @@ const getPostsWithTags = `
                LIMIT 1) as is_friend ON 1=1 
 
 
-            LEFT JOIN users as posts_user on posts.author = users.id
+            LEFT JOIN users as posts_user on posts.author = posts_user.id
             LEFT JOIN post_tags on posts.id = post_tags.post_id
             LEFT JOIN tags on post_tags.tag_id = tags.id
             LEFT JOIN threads as parent_thread on parent_thread.id = posts.parent_thread
@@ -47,7 +69,7 @@ const getPostsWithTags = `
             LEFT JOIN content_warnings on post_warnings.warning_id = content_warnings.id
             
             LEFT JOIN posts as first_post_in_thread on first_post_in_thread.parent_post is NULL AND first_post_in_thread.parent_thread = posts.parent_thread AND first_post_in_thread.id != posts.id
-            LEFT JOIN users as first_post_in_thread_user on first_post_in_thread.author = users.id
+            LEFT JOIN users as first_post_in_thread_user on first_post_in_thread.author = first_post_in_thread_user.id
             LEFT JOIN post_tags as first_post_post_tags on first_post_in_thread.id = first_post_post_tags.post_id
             LEFT JOIN tags as first_post_tags on first_post_post_tags.tag_id = first_post_tags.id 
             LEFT JOIN posts as first_post_child_posts on first_post_child_posts.parent_post = first_post_in_thread.id 
@@ -65,7 +87,11 @@ const getPostsWithTags = `
             post_identity.*,
             is_friend.friend,
             post_secret_identity.*,
-            first_post_secret_identity.*
+            first_post_secret_identity.*,
+            posts_user.*,
+            first_post_in_thread_user.*,
+            posts_user.username,
+            posts_user.avatar_reference_id
             
       ) as posts_with_tags    
     WHERE
